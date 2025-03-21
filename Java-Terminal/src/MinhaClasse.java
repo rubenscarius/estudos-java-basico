199623cc05436271c45bcf43a4dc79cc50cabcf7
rubenscarius <rubenscarius@msn.com>
2025-03-20 22:32:42 -0300

    #

--- Java-Terminal/src/MinhaClasse.java
+++ Java-Terminal/src/MinhaClasse.java
@@ -4,20 +4,24 @@ import java.util.Scanner;
 public class MinhaClasse {
     public static void main(String[] args) throws Exception {
 
-        // String nome = args [0];
+        String nome;
+        String sobrenome;
+        int idade;
+        double altura;
+        try ( // String nome = args [0];
         // String sobrenome = args [1];
         // int idade = Integer.valueOf(args[2]);
         // double altura = Double.valueOf(args[3]);
-        Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
-        System.out.println("Digite seu nome");
-        String nome = scanner.next();
-        System.out.println("Digite seu sobrenome");
-        String sobrenome = scanner.next();
-        System.out.println("Digite sua idade");
-        int idade = scanner.nextInt();
-        System.out.println("Digite sua altura");
-        double altura = scanner.nextDouble();
-        scanner.close();
+                Scanner scanner = new Scanner(System.in).useLocale(Locale.US)) {
+            System.out.println("Digite seu nome");
+            nome = scanner.next();
+            System.out.println("Digite seu sobrenome");
+            sobrenome = scanner.next();
+            System.out.println("Digite sua idade");
+            idade = scanner.nextInt();
+            System.out.println("Digite sua altura");
+            altura = scanner.nextDouble();
+        }
 
         System.out.println("------------------------------------");
         System.out.println("Ola, me chamo " + nome + " " + sobrenome);
